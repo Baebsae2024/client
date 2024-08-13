5dfaae87dc93f9f029a3c7e9c0536244500de55a
rlotr02 <0314kelee@naver.com>
2024-08-13 23:11:57 +0900

    feat: 예상 지원금 찾기 UI 제작

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -1,12 +1,143 @@
+import MiniButton from '@/components/button/MiniButton';
 import * as S from '@/styles/support/SupportPageStyle';
 import BigLongLine from '@assets/icons/BigLongLine.svg?react';
+import RadioIcon from '@assets/icons/Radio.svg?react';
+import RadioFillIcon from '@assets/icons/RadioFill.svg?react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type QuestionRadioProps = {
+  question: string;
+  selectedValue: boolean;
+  setSelectedValue: (value: boolean) => void;
+  options: { label: string; value: boolean }[];
+};
 
 const SupportPage = () => {
+  const navigate = useNavigate();
+  const [age, setAge] = useState(''); //나이
+  const [isForeigner, setIsForeigner] = useState(false); //외국인 true, 내국인 false
+  const [job, setJob] = useState(''); //직업
+  const [isMarried, setIsMarried] = useState(false); //결혼 여부
+  const [haveChildren, setHaveChildren] = useState(false); //자녀 여부
+  const [numberOfChildren, setNumberOfChildren] = useState(''); //자녀 수
+  const [childrenAges, setChildrenAges] = useState(''); //자녀 나이
+  const [monthlyIncome, setMonthlyIncome] = useState(''); //월 소득
+  const [isVeteran, setIsVeteran] = useState(false); //유공자 여부
+  const [hasEmergency, setHasEmergency] = useState(false); //긴급 및 재난 사고 여부
+  const [hasHospitalization, setHasHospitalization] = useState(false); //질병으로 입원 여부
+
+  const QuestionRadio = ({
+    question,
+    selectedValue,
+    setSelectedValue,
+    options,
+  }: QuestionRadioProps) => (
+    <>
+      <h2>{question}</h2>
+      <S.RadioWrap>
+        {options.map(option => (
+          <div
+            key={option.label}
+            onClick={() => setSelectedValue(option.value)}
+          >
+            {selectedValue === option.value ? <RadioFillIcon /> : <RadioIcon />}
+            {option.label}
+          </div>
+        ))}
+      </S.RadioWrap>
+    </>
+  );
+
+  const handleResult = () => {
+    navigate('/support/result');
+  };
+
   return (
     <S.Container>
       <h1>예상 지원금 찾기</h1>
-      <p>최대 000만원까지 받을 수 있어요!</p>
+      <p>질문에 답하고 지원금을 추천받으세요!</p>
       <BigLongLine style={{ marginTop: 65, marginBottom: 65 }} />
+      <h2>Q1. 나이가 어떻게 되시나요?</h2>
+      <input value={age} onChange={event => setAge(event.target.value)} />
+      <QuestionRadio
+        question="Q2. 귀하는 내국인인가요? 외국인인가요?"
+        selectedValue={isForeigner}
+        setSelectedValue={setIsForeigner}
+        options={[
+          { label: '내국인', value: false },
+          { label: '외국인', value: true },
+        ]}
+      />
+      <h2>Q3. 현재 직업은 무엇인가요?</h2>
+      <input value={job} onChange={event => setJob(event.target.value)} />
+      <QuestionRadio
+        question="Q4. 결혼하셨나요?"
+        selectedValue={isMarried}
+        setSelectedValue={setIsMarried}
+        options={[
+          { label: '네', value: true },
+          { label: '아니요', value: false },
+        ]}
+      />
+      <QuestionRadio
+        question="Q5. 자녀가 있으신가요?"
+        selectedValue={haveChildren}
+        setSelectedValue={setHaveChildren}
+        options={[
+          { label: '네', value: true },
+          { label: '아니요', value: false },
+        ]}
+      />
+      {haveChildren && (
+        <>
+          <h2>Q6. 자녀 수는 몇 명인가요?</h2>
+          <input
+            value={numberOfChildren}
+            onChange={event => setNumberOfChildren(event.target.value)}
+          />
+          <h2>Q7. 자녀의 나이는 어떻게 되나요?</h2>
+          <input
+            value={childrenAges}
+            onChange={event => setChildrenAges(event.target.value)}
+          />
+        </>
+      )}
+      <h2>Q8. 귀하의 월 소득은 어느 정도인가요?</h2>
+      <input
+        value={monthlyIncome}
+        onChange={event => setMonthlyIncome(event.target.value)}
+      />
+      <QuestionRadio
+        question="Q9. 유공자이신가요?"
+        selectedValue={isVeteran}
+        setSelectedValue={setIsVeteran}
+        options={[
+          { label: '네', value: true },
+          { label: '아니요', value: false },
+        ]}
+      />
+      <QuestionRadio
+        question="Q10. 최근에 긴급한 일이나 재난 사고를 겪으셨나요?"
+        selectedValue={hasEmergency}
+        setSelectedValue={setHasEmergency}
+        options={[
+          { label: '네', value: true },
+          { label: '아니요', value: false },
+        ]}
+      />
+      <QuestionRadio
+        question="Q11. 최근에 질병으로 입원하신 적이 있나요?"
+        selectedValue={hasHospitalization}
+        setSelectedValue={setHasHospitalization}
+        options={[
+          { label: '네', value: true },
+          { label: '아니요', value: false },
+        ]}
+      />
+      <S.BtnWrap>
+        <MiniButton idDisabled={false} text="결과보기" onClick={handleResult} />
+      </S.BtnWrap>
     </S.Container>
   );
 };
--- src/styles/support/SupportPageStyle.ts
+++ src/styles/support/SupportPageStyle.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   padding-top: 78px;
   padding-bottom: 115px;
 
@@ -19,4 +18,43 @@ export const Container = styled.div`
     font: var(--M28);
     color: #000;
   }
+
+  > h2 {
+    font: var(--M25);
+    color: #000;
+    margin-bottom: 16px;
+  }
+
+  > input {
+    font: var(--M20-23);
+    color: #000;
+    padding: 15px 20px;
+    border: 3px solid #c0e0e2;
+    width: 482px;
+    margin-bottom: 30px;
+  }
+`;
+
+export const RadioWrap = styled.div`
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  margin-bottom: 50px;
+
+  > div {
+    cursor: pointer;
+    font: var(--M20-23);
+    color: #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    user-select: none;
+  }
+`;
+
+export const BtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 13px;
 `;
